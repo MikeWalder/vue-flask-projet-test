8f519f858cfb59dca7f1edc6873d9625c1779a5c
MikeWalder <mike_walder@hotmail.fr>
2022-07-17 23:45:04 +0200

    Add Game adding with axios, elements of modal with Vuetify

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 
 # CORS = Cross Origin Request : for API calls basically
 
@@ -19,6 +20,71 @@ def greetings():
 @app.route('/shark', methods=['GET'])
 def shark():
     return("This is a message from the backend ! And it works !! ")
+
+
+GAMES = [
+    {
+        'title': 'GTA San Andreas',
+        'genre': 'action',
+        'played': True,
+    },
+    {
+        'title': 'GTA Vie City',
+        'genre': 'action',
+        'played': False,
+    }
+
+]
+
+# GET and POST route handler
+@app.route('/games', methods=['GET', 'POST'])
+def all_games():
+    response_object = {'status': 'success'} # default response when an HTTP request is successfull 
+    
+    if request.method == "POST":
+        post_data = request.get_json()
+        GAMES.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'genre': post_data.get('genre'),
+            'played': post_data.get('played')})
+        response_object['message'] = 'Game Added !'
+    else: 
+        response_object['games'] = GAMES
+    
+    return jsonify(response_object)
+    
+    """
+    return jsonify({
+        'games': GAMES,
+        'status': 'success'
+    })
+    """
+
+# The PUT and DELETE route handler
+@app.route('/games/<game_id>', methods = ['PUT'])
+def single_game(game_id):
+    response_object = {'status' : 'success'}
+    if request.method == "PUT":
+        post_data = request.get_json()
+        remove_game(game_id)
+        GAMES.append({
+            id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'genre': post_data.get('genre'),
+            'played': post_data.get('played')
+        })
+        response_object['message'] = 'Game Updated !'
+    return jsonify(response_object)
+
+# Remove the game to update
+def remove_game(game_id):
+    for game in GAMES:
+        if game['id'] == game_id:
+            GAMES.remove(game)
+            return True
+    return False
+
 
 if __name__ == "__main__":
     app.run(debug=True)
